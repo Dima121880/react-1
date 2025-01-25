db75e13fa7f295dd1b8db3959e9231c937b73b64
Dima121880 <surguladze.dima33@gmail.com>
2025-01-25 20:02:06 +0400

    add firstname and lastname

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+// function App() {
+//   const [firstname, setFirstname] = useState("");
+//   const [lastname, setlastname] = useState("");
+
+//   const clear = () => {
+//     setFirstname("");
+//     setlastname("");
+//   };
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         volue={Firstname}
+//         placeholder="Firstname"
+//         onChange={(e) => setFirstname(e.target.volue)}
+//       />
+//       <br />
+//       <input
+//         type="text"
+//         volue={lastname}
+//         placeholder="lastname"
+//         onChange={(e) => setlastname(e.target.volue)}
+//       />
+//       <br />
+//       {firstname} {lastname}
+//       <button onClick={() => clear}>clear</button>
+//     </>
+//   );
+// }
 function App() {
-  const [count, setCount] = useState(0)
+  const [Firstname, setFirstname] = useState("");
+  const [lastname, setlastname] = useState("");
 
+  const clear = () => {
+    setFirstname("");
+    setlastname("");
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        value={Firstname}
+        placeholder="Firstname"
+        onChange={(e) => setFirstname(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        value={lastname}
+        placeholder="lastname"
+        onChange={(e) => setlastname(e.target.value)}
+      />
+      <br />
+      {Firstname} {lastname}
+      <br />
+      <button onClick={() => clear()}>clear</button>
     </>
-  )
+  );
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vite.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.jsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // );
 }
 
-export default App
+export default App;
